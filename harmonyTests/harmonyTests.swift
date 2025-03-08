a7a6cafd53eefdde9fa77f29d09c48aae2de72ce
Baptiste Broc <baptiste.broc.pro@gmail.com>
2025-03-08 13:38:52 +0100

    Initial Commit

---
+++ harmonyTests/harmonyTests.swift
@@ -0,0 +1,17 @@
+//
+//  harmonyTests.swift
+//  harmonyTests
+//
+//  Created by Baptiste Broc on 08/03/2025.
+//
+
+import Testing
+@testable import harmony
+
+struct harmonyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
